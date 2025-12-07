57c1caf05a44d34d1e66366185e1103c73a81466
Abhishek <abhishekyadav88846@gmail.com>
2025-12-07 21:15:42 +0530

    add gitignore

--- 7.cpp
+++ 7.cpp
@@ -33,32 +33,61 @@ vector<string> get_input() {
 int main() {
   vector<string> input = get_input();
 
-  queue<pair<int, int>> Q;
+  vector<pair<int, int>> Q;
+  set<pair<int, int>> visited;
 
   int N = input.size(), M = input[0].size();
+  vector<vector<long long>> dp(N, vector<long long>(M, -1));
+
+  auto add = [&](int x, int y) {
+    if (visited.count({x, y})) {
+      return;
+    }
+    visited.insert({x, y});
+    Q.emplace_back(x, y);
+  };
+
+  // part - 2
+  function<long long(int, int)> dfs = [&](int x, int y) {
+    if (x >= N || y >= N || y < 0) {
+      return 1LL;
+    }
+    if (dp[x][y] != -1) {
+      return dp[x][y];
+    }
+    if (input[x][y] == '^') {
+      return dp[x][y] = dfs(x + 1, y - 1) + dfs(x + 1, y + 1);
+    } else {
+      return dp[x][y] = dfs(x + 1, y);
+    }
+  };
 
   for (int i = 0; i < M; ++i) {
     if (input[0][i] == 'S') {
-      Q.emplace(0, i);
+      add(0, i);
+      cout << "Second : ";
+      cout << dfs(0, i) << '\n';
     }
   }
 
   int res = 0;
 
   while (!Q.empty()) {
-    auto t = Q.pop();
+    auto t = Q.back();
+    Q.pop_back();
     if (t.first >= N || t.second >= M || t.second < 0) {
-      res++;
       continue;
     }
     if (input[t.first][t.second] == '^') {
-      Q.emplace(t.first + 1, t.second - 1);
-      Q.emplace(t.first + 1, t.second + 1);
+      res++;
+      add(t.first + 1, t.second - 1);
+      add(t.first + 1, t.second + 1);
     } else {
-      Q.emplace(t.first + 1, t.second);
+      add(t.first + 1, t.second);
     }
   }
 
+  // first
   cout << res << '\n';
 
   return 0;
